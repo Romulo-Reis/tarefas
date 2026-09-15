6e479956132ca7532073ea96165ccf702eb687e3
Romulo-Reis <romuloreistavares@hotmail.com>
2026-09-15 20:47:57 -0300

    feat: Configura o provider de autenticação.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,14 @@
 import '../../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Header } from '../components/header'
+import { SessionProvider } from "next-auth/react"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <SessionProvider session={pageProps.session}>
       <Header/>
       <Component {...pageProps} />
-    </>
+    </SessionProvider>
   )
 }
 export default MyApp
