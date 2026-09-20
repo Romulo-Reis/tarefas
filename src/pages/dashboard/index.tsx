0fc7311969e482a1a7b32b45c2803a68579a8ff6
Romulo-Reis <romuloreistavares@hotmail.com>
2026-09-20 19:19:03 -0300

    feat: Cria o layout da página de dashboard.

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,5 +1,11 @@
 import styles from './styles.module.css';
 import Head from "next/head";
+import type { GetServerSideProps } from "next";
+
+import { getSession } from "next-auth/react";
+import { Textarea } from '../../components/textarea';
+import { FiShare2 } from 'react-icons/fi';
+import { FaTrash } from 'react-icons/fa';
 
 export default function Dashboard() {
     return (
@@ -8,7 +14,70 @@ export default function Dashboard() {
                 <title>Meu painel de tarefas</title>
             </Head>
 
-            <h1>Página Painel</h1>
+            <main className={styles.main}>
+                <section className={styles.content}>
+                    <div className={styles.contentForm}>
+                        <h1 className={styles.title}>Qual sua tarefa?</h1>
+                        <form>
+                            <Textarea 
+                                placeholder="Digite qual sua tarefa..."
+                            />
+                            <div className={styles.checkboxAre}>
+                                <input 
+                                    type='checkbox'
+                                    className={styles.checkbox}  
+                                />
+                                <label>Deixar tarefa pública?</label>
+                            </div>
+                            <button className={styles.button} type="submit">
+                                Registrar
+                            </button>
+                        </form>
+                    </div>
+                </section>
+
+                <section className={styles.taskContainer}>
+                    <h1>Minhas tarefas</h1>
+                    <article className={styles.task}>
+                        <div className={styles.tagContainer}>
+                            <label className={styles.tag}>PUBLICADO</label>
+                            <button className={styles.shareButton}>
+                                <FiShare2
+                                    size={22}
+                                    color="#3183ff"
+                                />
+                            </button>
+                        </div>
+                        <div className={styles.taskContent}>
+                            <p>Minha primeira tarefa de exemplo show demais!</p>
+                            <button className={styles.trashButton}>
+                                <FaTrash
+                                    size={24}
+                                    color="#ea3140"
+                                />
+                            </button>
+                        </div>
+                    </article>
+                </section>
+            </main>
         </div>
     )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({req}) => {
+
+    const session = await getSession({req});
+
+    if(!session?.user) {
+        return {
+            redirect: {
+                destination: "/",
+                permanent: false,
+            },
+        }
+    }
+
+    return {
+        props: {}
+    }
 }
